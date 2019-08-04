d0a602fdd0399bc747971643b7255669ec4b6e3c
Marc Ochsner <marcaochsner@gmail.com>
2019-08-04 16:04:39 -0500

    remove resource routes (source broken)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root "home#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  # resources :users
+  # resources :applications
 
   # Example resource route with options:
   #   resources :products do
