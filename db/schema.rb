b1766f863edbcec5fedf10eb9c6e148ddb12a842
Marc Ochsner <marcaochsner@gmail.com>
2019-08-04 14:52:39 -0500

    Core tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190804062602) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "Color"
+    t.string   "DefaultStatus"
+    t.string   "Link"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "login"
+    t.string   "Password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,18 @@ user_list.each do |login,password|
   User.create( login: login, Password: password )
 end
 
-apps = Application.create([
-{ name: 'Google', description: 'Search Engine', Color: 'Red', DefaultStatus: 'Yes', Link: 'http://www.google.com'},
-{ name: 'Wisc', description: 'UW homepage', Color: 'Blue', DefaultStatus: 'No', Link: 'http://www.wisc.edu'},
-{ name: 'GLBRC', description: 'Great Lakes Bioenergy Research Center', Color: 'Yellow', DefaultStatus: 'Yes', Link: 'http://www.glbrc.org'},
-{ name: 'WEI', description: 'Wisconsin Energy Institute', Color: 'Green', DefaultStatus: 'No', Link: 'https://energy.wisc.edu/'},
-{ name: 'Twitter', description: 'Twitter', Color: 'Purple', DefaultStatus: 'No', Link: 'https://twitter.com'}])
+#apps = Application.create([
+#{ name: 'Google', description: 'Search Engine', Color: 'Red', DefaultStatus: 'Yes', Link: 'http://www.google.com'},
+#{ name: 'Wisc', description: 'UW homepage', Color: 'Blue', DefaultStatus: 'No', Link: 'http://www.wisc.edu'},
+#{ name: 'GLBRC', description: 'Great Lakes Bioenergy Research Center', Color: 'Yellow', DefaultStatus: 'Yes', Link: 'http://www.glbrc.org'},
+#{ name: 'WEI', description: 'Wisconsin Energy Institute', Color: 'Green', DefaultStatus: 'No', Link: 'https://energy.wisc.edu/'},
+#{ name: 'Twitter', description: 'Twitter', Color: 'Purple', DefaultStatus: 'No', Link: 'https://twitter.com'}])
+Application.create( name: 'Google', description: 'Search Engine', Color: 'Red', DefaultStatus: 'Yes', Link: 'http://www.google.com')
+Application.create( name: 'Wisc', description: 'UW homepage', Color: 'Blue', DefaultStatus: 'No', Link: 'http://www.wisc.edu')
+Application.create( name: 'GLBRC', description: 'Great Lakes Bioenergy Research Center', Color: 'Yellow', DefaultStatus: 'Yes', Link: 'http://www.glbrc.org')
+Application.create( name: 'WEI', description: 'Wisconsin Energy Institute', Color: 'Green', DefaultStatus: 'No', Link: 'https://energy.wisc.edu/')
+Application.create( name: 'Twitter', description: 'Twitter', Color: 'Purple', DefaultStatus: 'No', Link: 'https://twitter.com')
+
 
 #country_list = [
 #  [ "Germany", 81831000 ],
