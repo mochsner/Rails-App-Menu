ba7747bca647f58939a4384093594e7703fafad4
Marc Ochsner <marcaochsner@gmail.com>
2019-08-05 01:11:34 -0500

    :construction:

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' , name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+=begin================================================================================
+	Model: User
+	View: NULL
+	Controller: NULL
+
+	Columns:
+		login
+		Password
+=end
 user_list = [
 	[ "user1","glbrcpass" ],
 	[ "user2","glbrcpss" ],
@@ -17,26 +26,47 @@ user_list.each do |login,password|
   User.create( login: login, Password: password )
 end
 
-#apps = App.create([
-#{ name: 'Google', description: 'Search Engine', Color: 'Red', DefaultStatus: 'Yes', Link: 'http://www.google.com'},
-#{ name: 'Wisc', description: 'UW homepage', Color: 'Blue', DefaultStatus: 'No', Link: 'http://www.wisc.edu'},
-#{ name: 'GLBRC', description: 'Great Lakes Bioenergy Research Center', Color: 'Yellow', DefaultStatus: 'Yes', Link: 'http://www.glbrc.org'},
-#{ name: 'WEI', description: 'Wisconsin Energy Institute', Color: 'Green', DefaultStatus: 'No', Link: 'https://energy.wisc.edu/'},
-#{ name: 'Twitter', description: 'Twitter', Color: 'Purple', DefaultStatus: 'No', Link: 'https://twitter.com'}])
-App.create( name: 'Google', description: 'Search Engine', Color: 'Red', DefaultStatus: 'Yes', Link: 'http://www.google.com')
-App.create( name: 'Wisc', description: 'UW homepage', Color: 'Blue', DefaultStatus: 'No', Link: 'http://www.wisc.edu')
-App.create( name: 'GLBRC', description: 'Great Lakes Bioenergy Research Center', Color: 'Yellow', DefaultStatus: 'Yes', Link: 'http://www.glbrc.org')
-App.create( name: 'WEI', description: 'Wisconsin Energy Institute', Color: 'Green', DefaultStatus: 'No', Link: 'https://energy.wisc.edu/')
-App.create( name: 'Twitter', description: 'Twitter', Color: 'Purple', DefaultStatus: 'No', Link: 'https://twitter.com')
+=begin================================================================================
+	Model: App
+	View: app.html.erb
+	Controller: NULL
 
+	Columns:
+		name
+		description
+		Color
+		DefaultStatus
+		Link
+=end
+app_list = [
+	['Google','Search Engine','Red','Yes','http://www.google.com')
+	['Wisc','UW homepage','Blue','No','http://www.wisc.edu')
+	['GLBRC','Great Lakes Bioenergy Research Center','Yellow','Yes','http://www.glbrc.org')
+	['WEI','Wisconsin Energy Institute','Green','No','https://energy.wisc.edu/')
+	['Twitter','Twitter','Purple','No','https://twitter.com')
+]
+app_list.each do |name,description,color,defaultstatus,link|
+  User.create( name:name, description:description, Color:color, DefaultStatus:defaultstatus, Link:link )
+end
 
-#country_list = [
-#  [ "Germany", 81831000 ],
-#  [ "France", 65447374 ],
-#  [ "Belgium", 10839905 ],
-#  [ "Netherlands", 16680000 ]
-#]
-#
-#country_list.each do |name, population|
-#  Country.create( name: name, population: population )
-#end
+=begin================================================================================
+	Model: UserApps
+	View: userapp
+	Controller: 
+
+	Columns:
+		UserApps_ID: #
+		User_ID=User.User_ID 
+		DECIDE (based on front-end/view limitations):
+			App_ID_0 = App.ID (literal)
+			App_ID_1 = App.ID (literal)
+			App_ID_... = App.ID (literal)
+			App_ID_N-1 = App.ID (literal)
+
+			~OR~
+
+			App_ID (literal) = 0
+			App_ID (literal) = 1
+			App_ID (literal) = ...
+			App_ID (literal) = N-1
+=end
