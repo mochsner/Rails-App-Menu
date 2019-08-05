19e7d6f92ddd2fc11687ca55f65b2e9b24b53bf6
Marc Ochsner <marcaochsner@gmail.com>
2019-08-05 03:20:42 -0500

    :construction: Testing changes for new model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,35 @@
 class HomeController < ApplicationController
-    def index
+
+    def test
+      return render_userapps
+    end
+
+    def render_apps
       @apps = App.all 
-      render json: @apps.to_json(only: [:name, :description, :Color, :DefaultStatus, :Link])
-    #   @users = User.all
-    #   render json: @users.to_json(only: [:login, :Password])
+      render json: @apps.to_json(only: [:id, :name, :description, :Color, :DefaultStatus, :Link])
     end 
+
+    def render_users
+      @users = User.all
+      render json: @users.to_json(only: [:id, :login, :Password])
+    end
+
+    def render_userapps
+      @userapps = UserApp.all
+      render json: @userapps.to_json()
+    end
+
+    def get_apps
+      @apps = App.all 
+      return @apps
+    end 
+
+    def get_users
+      @users = User.all
+      return @users
+    end
+
+    def all_userapps
+      @userapps = get_apps 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :users
+  resources :apps
+
+  get 'users' => 'home#index'
+
+  root "home#test"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root "home#index"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
